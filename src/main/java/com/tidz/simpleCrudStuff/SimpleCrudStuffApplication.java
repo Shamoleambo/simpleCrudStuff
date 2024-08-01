f4e0fae75ed4a41415730a9f6cd3f3ec8d7dec24
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-01 12:42:38 -0300

    first commit

---
+++ src/main/java/com/tidz/simpleCrudStuff/SimpleCrudStuffApplication.java
@@ -0,0 +1,13 @@
+package com.tidz.simpleCrudStuff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudStuffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCrudStuffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tidz/simpleCrudStuff/SimpleCrudStuffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidz.simpleCrudStuff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudStuffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
